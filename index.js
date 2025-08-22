ad9385b287b5f191b6383ce57467a8ff9034913b
profremontpredst <mikhailfeskovavangard@gmail.com>
2025-08-22 12:11:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Раздаём всё из public/
+app.use(express.static(path.join(__dirname, "public")));
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
